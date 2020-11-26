f952356329d95f81604e6b425d3a899c585e8690
garrisonhh <garrisonhh@pm.me>
2020-11-26 16:46:43 -0500

    big primes improvement

---
+++ julia/primes.jl
@@ -0,0 +1,1 @@
+[println(n) for n=2:99if(2^BigInt(n-1)%n==1)|(n≡2)]
--- python/prime.py
+++ python/prime.py
@@ -1,2 +1,5 @@
-# [print(n)for n in[2,*range(3,99,2)]if 0x1129A64B4CB6F>>int((n-1)/2)&1] # this isn't the most golfed solution but god damn is it not hilarious
-[print(n)for n in range(2,99)if sum(n%x<1for x in range(2,n))<1] # most golfed currently
+# 64 chars; calculates true primes
+# [print(n)for n in range(2,99)if sum(n%x<1for x in range(2,n))<1]
+
+# 54 chars; calculates probable primes
+[print(n)for n in range(2,99)if n==2or 2**(n-1)%n==1]
