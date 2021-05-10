6532fa953aa55b30057b7200dbea2048a3cb6b47
garrison hinson-hasty <71951273+garrisonhh@users.noreply.github.com>
2021-05-09 21:42:54 -0400

    made 110.py one line

--- python/110.py
+++ python/110.py
@@ -1,5 +1,1 @@
-n=100
-z='0'
-j=''.join
-s=z*n+"10"
-exec("print(j(' █'[c>z]for c in s[1:-1]));s=z+j(str(110>>int(s[i:i+3],2)&1)for i in range(n))+z;"*n)
+n=100;z='0';j=''.join;s=z*n+"10";exec("print(j(' █'[c>z]for c in s[1:-1]));s=z+j(str(110>>int(s[i:i+3],2)&1)for i in range(n))+z;"*n)
